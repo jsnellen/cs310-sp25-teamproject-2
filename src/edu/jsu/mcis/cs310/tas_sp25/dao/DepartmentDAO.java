ced7cd14d2ab9e0c2e031878c367f8c5309987f0
Dillon Firman <dfirman@stu.jsu.edu>
2025-03-03 12:55:24 -0600

    Fixed some errors for DepartmentDAO

--- src/edu/jsu/mcis/cs310/tas_sp25/dao/DepartmentDAO.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/dao/DepartmentDAO.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package edu.jsu.mcis.cs310.tas_sp25;
+package edu.jsu.mcis.cs310.tas_sp25.dao;
 
+import edu.jsu.mcis.cs310.tas_sp25.Department;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
