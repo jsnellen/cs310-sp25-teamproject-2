4b4a51144d77c5d26e730612a117dd1f0333a41c
Dillon Firman <dfirman@stu.jsu.edu>
2025-03-12 12:43:07 -0500

    Hopefully fixed and adjust the Employee.java for the final time

--- src/edu/jsu/mcis/cs310/tas_sp25/Employee.java
+++ src/edu/jsu/mcis/cs310/tas_sp25/Employee.java
@@ -1,91 +1,131 @@
 package edu.jsu.mcis.cs310.tas_sp25;
 
 import java.time.LocalDateTime;
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 /**
- *
- * @author dillon
+ * Employee class is primarily used for storing and sharing data related to any
+ * employee within the database.
+ * 
+ * @author Dillon Firman
  */
+
 public class Employee {
+
+    private final Integer id;
+    private final String firstName, middleName, lastName;
+    private final LocalDateTime active;
+    private final Shift shift;
+    private final Badge badge;
+    private final Department department;
+    private final EmployeeType type;
     
-    //Making variables
-    private int id;
-    private String firstname;
-    private String middlename;
-    private String lastname;
-    private LocalDateTime active;
-    private Badge badge;
-    private Department department;
-    private Shift shift;
-    private EmployeeType employeeType;
-    
-    //Constructors
-    public Employee(int id, String firstname, String middlename, String lastname, LocalDateTime active, Badge badge, Department department, Shift shift, EmployeeType employeeType){
-        this.id = id;
-        this.firstname = firstname;
-        this.middlename = middlename;
-        this.lastname = lastname;
-        this.active = active;
-        this.badge = badge;
-        this.department = department;
-        this.shift = shift;
-        this.employeeType = employeeType;
+   
+
+    public Employee(HashMap<String, Object> employee) {
+        
+        this.id = (Integer) employee.get("id");
+        this.firstName = (String) employee.get("firstName");
+        this.middleName = (String) employee.get("middleName");
+        this.lastName = (String) employee.get("lastName");
+        this.active = (LocalDateTime) employee.get("active");
+        this.badge = (Badge) employee.get("badge");
+        this.department = (Department) employee.get("department");
+        this.shift = (Shift) employee.get("shift");
+        this.type = (EmployeeType) employee.get("employeeType");
     }
-    
-    //Getters
-    public int getId() {
+
+    /**
+     * @return id
+     */
+    public Integer getId() {
+        
         return id;
     }
-
-    public String getFirstname() {
-        return firstname;
+    
+    /**
+     * @return firstName
+     */
+    public String getFirstName() {
+        
+        return firstName;
     }
-
-    public String getMiddlename() {
-        return middlename;
+    
+    /**
+     * @return middleName
+     */
+    public String getMiddleName() {
+        
+        return middleName;
     }
-
-    public String getLastname() {
-        return lastname;
+    
+    /**
+     * @return lastName
+     */
+    public String getLastName() {
+        
+        return lastName;
     }
-
+    
+    /**
+     * @return active time
+     */
     public LocalDateTime getActive() {
+        
         return active;
     }
-
+    
+    /**
+     * @return badge
+     */
     public Badge getBadge() {
+        
         return badge;
     }
-
+    
+    /**
+     * @return department
+     */
     public Department getDepartment() {
+        
         return department;
     }
-
+    
+    /**
+     * @return shift
+     */
     public Shift getShift() {
+        
         return shift;
     }
-
-    public EmployeeType getEmployeeType() {
-        return employeeType;
+    
+    /**
+     * @return type
+     */
+    public EmployeeType getType() {
+        
+        return type;
     }
     
-    //Override toString() method to match
+    /**
+     * Overrides the toString method
+     * @return the employee fields formatted to accommodate a test
+     */
     @Override
     public String toString() {
+        
         StringBuilder s = new StringBuilder();
-   
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        String formattedActiveDate = active.format(formatter);
+
         s.append("ID #").append(getId()).append(": ");
-        s.append(getLastname()).append(", ").append(getFirstname()).append(' ').append(getMiddlename());
-        s.append(" (#").append(badge.getId()).append("), Type: ").append(getEmployeeType());
+        s.append(getLastName()).append(", ").append(getFirstName()).append(' ').append(getMiddleName());
+        s.append(" (#").append(badge.getId()).append("), Type: ").append(getType());
         s.append(", Department: ").append(getDepartment().getDescription());
+        s.append(", Active: ").append(formattedActiveDate);
         
         return s.toString();
-
+        
     }
-    
 }
