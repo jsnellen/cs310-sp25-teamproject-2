ec68b2262b4a7c273013e8199a94a1d78d88cbbf
Dillon Firman <dfirman@stu.jsu.edu>
2025-03-12 13:53:49 -0500

    Created the Absenteeism model class

---
+++ src/edu/jsu/mcis/cs310/tas_sp25/Absenteeism.java
@@ -0,0 +1,69 @@
+package edu.jsu.mcis.cs310.tas_sp25;
+
+import java.time.LocalDate;
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author Dillon Firman
+ */
+
+public class Absenteeism {
+
+    private final Employee employee;
+    private final LocalDate localDate;
+    private final BigDecimal bigDecimal;
+
+    /**
+     *
+     * @param employee
+     * @param localDate
+     * @param bigDecimal
+     */
+    public Absenteeism(Employee employee, LocalDate localDate, BigDecimal bigDecimal) {
+        this.employee = employee;
+        this.localDate = localDate;
+        this.bigDecimal = bigDecimal;
+    }
+
+    /**
+     *
+     * @return employee
+     */
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    /**
+     *
+     * @return localDate
+     */
+    public LocalDate getLocalDate() {
+        return localDate;
+    }
+
+    /**
+     *
+     * @return bigDecimal
+     */
+    public BigDecimal getBigDecimal() {
+        return bigDecimal;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder s = new StringBuilder();
+        
+        // build string "#28DC3FB8 (Pay Period Starting 09-02-2018): 2.50%"
+        
+        s.append('#').append(employee.getBadge()).append(' ');
+        s.append("(Pay Period Starting ").append(getLocalDate()).append("): ");
+        s.append(getBigDecimal());
+
+        return s.toString();
+
+    }
+
+}
+
